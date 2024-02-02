3ffad78d3c2df23162830d56fbd248fe0da7f205
SnowIceApple <154928907+SnowIceApple@users.noreply.github.com>
2024-02-02 20:45:11 +0900

    upload

---
+++ inc/common.php
@@ -0,0 +1,28 @@
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no" />
+    <!-- <meta name="viewport" content="width=1760, user-scalable=yes, target-densitydpi=400" /> -->
+    <title>Portfolio</title>
+
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/mobile.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" as="style" crossorigin href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/static/pretendard.css">
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@200;300;400;500;600;700;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lugrasimo&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="./js/global.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    
+
+
---
+++ js/global.js
@@ -0,0 +1,137 @@
+
+
+
+$(document).ready(function(){
+  
+  AOS.init();
+
+    const swiper1 = new Swiper('.works_swiper', {
+    direction: 'horizontal',
+      loop: true,
+      speed: 1000,
+      slidesPerView: 1,
+      centeredSlides: true,
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true
+      },
+    //   autoplay: {
+    //     delay: 3000,
+    //     disableOnInteraction: false
+    //   },
+
+      navigation: {
+        nextEl: '.wks_btn.next',
+        prevEl: '.wks_btn.prev',
+      },
+
+      breakpoints:{
+        568: {
+            slidesPerView: 'auto',
+        },
+      },
+
+    });  
+
+
+
+  $(window).scroll(function(){
+
+    function mainScrollEffect(){
+        const scroll = $(this).scrollTop();
+        const {scrollHeight,clientHeight,scrollTop} = document.documentElement;
+        const scroll2 = scrollTop / (scrollHeight-clientHeight);
+        const upScroll = -scroll/4;
+        const upScroll2 = -scroll/3;
+        const upScroll3 = -scroll/20;
+        const upScroll4 = -scroll2 * 130;
+    
+        $('.scroll_up').css({'transform' : 'translate3d(0,'+ upScroll +'px,0)'});
+        $('.scroll_up2').css({'transform' : 'translate3d(0,'+ upScroll2 +'px,0)'});
+        $('.scroll_up3').css({'transform' : 'translate3d(0,'+ upScroll3 +'px,0)'});
+        $('.scroll_up4').css({'transform' : 'translate3d(0,'+ upScroll4 +'px,0)'});
+    
+        console.log(upScroll);
+    }
+
+    if($(window).scrollTop() < $('#intro').offset().top){
+        mainScrollEffect();
+    }
+
+    var span_txt_anchor = $('#visual').innerHeight() / 2;
+
+    if($(window).scrollTop() > span_txt_anchor){
+      $('.top_txt span').text('Top');
+    }
+    else{
+      $('.top_txt span').text('Hello!');
+    }
+});
+
+
+  $('.go_top').click(function(){
+    $('html, body').animate({ scrollTop: 0 }, 600);
+  });
+
+  $('.main_nav_open button').on('click', function(){
+    $('.main_nav').addClass('on');
+    $('body').addClass('fixed');
+  });
+
+  $('.main_nav_close button').on('click', function(){
+    $('.main_nav').removeClass('on');
+    $('body').removeClass('fixed');
+  });
+
+  $('.nav_link a').on('click', function(){
+    $('.main_nav').removeClass('on');
+    $('body').removeClass('fixed');
+  });
+
+  $('.multi_link').on('click', function(e){
+    $(this).toggleClass('active');
+  });
+
+  $(window).on('resize', function(){
+    if($(window).innerWidth() > 1280){
+        $('.multi_link').removeClass('active');
+    }
+  });
+
+  $('.main_contact ul li').on('mouseenter', function(){
+    $(this).removeClass('alarm_active');
+  });
+
+  $(document).on('mouseup', function(e){
+    var main_contact_pop = $('.main_contact ul li');
+    if(main_contact_pop.has(e.target).length === 0){
+        main_contact_pop.removeClass('alarm_active');
+    }
+  });
+
+  function alarm_on() {
+    var min = 10,
+      max = 30;
+    var randonNum = Math.floor(Math.random() * (max - min + 1) + min); 
+    $('.main_contact ul li').addClass('alarm_active');
+    setTimeout(alarm_on, randonNum * 1000);
+  }
+  
+  alarm_on()
+
+
+});
+
+$(window).on('load', function(){
+    $('.main_nav_open').addClass('pop_up');
+  });
+
+
+
+
+
+
+
+
+
+
